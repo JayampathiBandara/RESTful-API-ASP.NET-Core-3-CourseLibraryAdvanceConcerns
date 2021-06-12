45f593f39a97b59e80a2cdea6844201ca8752966
jayampathib <wmjb.weerasinghe@gmail.com>
2021-06-13 00:36:55 +0800

    2.2 Sorting Collection – comma , space as seperators| age is derived type  =>> 2.2.3 Property Mapping implementation

---
+++ CourseLibrary.API/Services/IPropertyMapping.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Services
+{
+    public interface IPropertyMapping
+    {
+        // This interface is called "MARKER Interface
+        // It doesn't have any method.
+        // this is used to initilize a list for
+        // class with geneic parameters
+    }
+}
---
+++ CourseLibrary.API/Services/PropertyMappingValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CourseLibrary.API.Services
+{
+    // used inside PropertyMappingService
+    public class PropertyMappingValue
+    {
+        public IEnumerable<string> DestinationProperties { get; private set; }
+
+        // if Age is come as property from client side, destination property is DateOfBirth
+        // Age Ascending order mean, Descending order of DateOfBirth
+        // To Identify it, when genrate Query, "Revert" property is used
+        public bool Revert { get; private set; }
+
+        public PropertyMappingValue(IEnumerable<string> destinationProperties,
+            bool revert = false)
+        {
+            DestinationProperties = destinationProperties
+                ?? throw new ArgumentNullException(nameof(destinationProperties));
+            Revert = revert;
+        }
+    }
+}
